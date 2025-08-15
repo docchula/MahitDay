6ac4a8e6bd810c3c316283603a7745ddfe9eea19
Nu <163573920+melonsugar01234@users.noreply.github.com>
2025-08-15 18:22:57 +0700

    feat: change Allow Certificate to false

--- utils/config.ts
+++ utils/config.ts
@@ -4,7 +4,7 @@ export const REGISTRATION_STAGE: RegisterStage = 'register';
 // open: open the website but user cannot register (use when registration is full)
 // close: close the website
 // register: user can register
-export const ALLOW_CERTIFICATE_PRINTING = true; // true or false (REGISTRATION_STAGE should be 'open' before setting to 'true')
+export const ALLOW_CERTIFICATE_PRINTING = false; // true or false (REGISTRATION_STAGE should be 'open' before setting to 'true')
 
 export const MAX_MEDTALK_STUDENT = 199; //ตั้ง 199 กันคนลงพร้อมกัน 2 คนเกินเป็น 201
 export const MAX_Gross_anatomy = 100;
